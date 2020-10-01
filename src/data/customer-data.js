14287115f7f0d7e39171b6c2f05b0614043200c9
chelseaconstruction <67573467+chelseaconstruction@users.noreply.github.com>
2020-10-01 12:59:26 -0700

    Update customer-data.js

--- src/data/customer-data.js
+++ src/data/customer-data.js
@@ -1,26 +1,26 @@
 export default [
   {
-    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+    title: 'Great work done in my house, very clean and responsible',
     content:
-      'Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Sit amet consectetur adipiscing elit duis.',
-    customerName: 'Jane Doe',
-    customerTitle: 'Director of Research and Data',
+      'Shelby needed her basement hanged and taped after a water leak had damaged her old sheetrock.',
+    customerName: 'Shelby R',
+    customerTitle: 'Happy client',
     customerImage: 'https://placeimg.com/150/150/people'
   },
   {
-    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+    title: 'You guys are the best, i feel like ive known you forever, i love my new ceilings',
     content:
-      'Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Sit amet consectetur adipiscing elit duis.',
-    customerName: 'John Doe',
-    customerTitle: 'Director of Research and Data',
+      'Mary did not want her old brocade texture anymore, we took off her old popcorn ceilings and made them smooth.',
+    customerName: 'Mary F',
+    customerTitle: 'Happy client',
     customerImage: 'https://placeimg.com/150/150/people'
   },
   {
-    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+    title: 'It was a pleasure with you, you are a gentleman and a scholar, will be in contact soon for more projects',
     content:
-      'Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Sit amet consectetur adipiscing elit duis.',
-    customerName: 'Jane Smith',
-    customerTitle: 'Director of Research and Data',
+      'David needed sheetrock hanged and taped in the kitchen immidietly due to  having a date aranged for the painters  ',
+    customerName: 'David L',
+    customerTitle: 'Happy contractor',
     customerImage: 'https://placeimg.com/150/150/people'
   }
 ];
