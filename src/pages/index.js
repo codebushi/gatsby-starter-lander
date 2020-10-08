43995efa376c23ddb823832ce5ac2d20680d8343
chelseaconstruction <67573467+chelseaconstruction@users.noreply.github.com>
2020-10-08 13:40:15 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -131,7 +131,7 @@ export default () => (
       </div>
     </section>
     <section className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
-      <h3 className="text-5xl font-semibold">Ready to create your ideal home ? 
+      <h3 className="text-5xl font-semibold">Ready to create your ideal home ?> 
       <p className="mt-8 text-xl font-light">
         Contact us! (503) 327 1232, Chelseapdxconstruction@gmail.com 
       </p>
