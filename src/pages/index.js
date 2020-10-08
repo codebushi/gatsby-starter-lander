7653f33de56e53ed6c706b452d85474ad8cca122
chelseaconstruction <67573467+chelseaconstruction@users.noreply.github.com>
2020-10-08 13:47:55 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -131,10 +131,9 @@ export default () => (
       </div>
     </section>
     <section className="container mx-auto my-20 py-24 bg-gray-200 rounded-lg text-center">
-      <h3 className="text-5xl font-semibold">Ready to create your ideal home ?> 
+      <h3 className="text-5xl font-semibold">Ready to create your ideal home ?> </h3>
       <p className="mt-8 text-xl font-light">
         Contact us! (503) 327 1232, Chelseapdxconstruction@gmail.com 
-      </h3>
       </p>
       <p className="mt-8">
         <Button size="xl">Get Started Now</Button>
