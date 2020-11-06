a5ee4f0c8cb72e0e7260205db2b49845420b12b2
chelseaconstruction <67573467+chelseaconstruction@users.noreply.github.com>
2020-11-05 18:36:50 -0800

    Update Footer.js

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -17,9 +17,6 @@ const Footer = () => (
           <li>
             <a href="https://instagram.com/Chelseaconstructionllc">Instagram</a>
           </li>
-          <li>
-            <a href=</a>
-          </li>
         </ul>
       </div>
     </div>
