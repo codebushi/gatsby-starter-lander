1c61c6ce71e2c27cfd08e85b427f9435c89b6fda
chelseaconstruction <67573467+chelseaconstruction@users.noreply.github.com>
2020-11-16 18:56:36 -0800

    Update Header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,13 +2,14 @@ import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import LogoIcon from '../../svg/LogoIcon';
 import Button from '../Button';
+import Logo from '../logo-CC.png
 
 const Header = () => (
   <header className="sticky top-0 bg-white shadow">
     <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
       <div className="flex items-center text-2xl">
         <div className="w-12 mr-3">
-          <LogoIcon />
+          <img src={logo} />
         </div>
         Chelsea PDX Construction
       </div>
