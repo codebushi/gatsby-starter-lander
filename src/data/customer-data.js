5598fde4515ab197bfed929eabc9be46401a3c50
chelseaconstruction <67573467+chelseaconstruction@users.noreply.github.com>
2020-11-16 20:01:48 -0800

    ppl images

--- src/data/customer-data.js
+++ src/data/customer-data.js
@@ -5,7 +5,7 @@ export default [
       'Shelby needed her basement hanged and taped after a water leak had damaged her old sheetrock.',
     customerName: 'Shelby R',
     customerTitle: 'Happy client',
-    customerImage: 'https://placeimg.com/640/480/any'
+    customerImage: 'https://placeimg.com/640/480/tech'
   },
   {
     title: 'You guys are the best, i feel like ive known you forever, i love my new ceilings',
@@ -13,7 +13,7 @@ export default [
       'Mary did not want her old brocade texture anymore, we took off her old popcorn ceilings and made them smooth.',
     customerName: 'Mary F',
     customerTitle: 'Happy client',
-    customerImage: 'https://placeimg.com/640/480/any'
+    customerImage: 'https://placeimg.com/640/480/nature'
   },
   {
     title: 'It was a pleasure with you, you are a gentleman and a scholar, will be in contact soon for more projects',
@@ -21,6 +21,6 @@ export default [
       'David needed sheetrock hanged and taped in the kitchen immidietly due to  having a date aranged for the painters  ',
     customerName: 'David L',
     customerTitle: 'Happy contractor',
-    customerImage: 'https://placeimg.com/640/480/any'
+    customerImage: 'https://placeimg.com/640/480/people'
   }
 ];
