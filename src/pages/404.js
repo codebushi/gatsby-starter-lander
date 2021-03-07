620cb76b57dac7d36d217ace41d2955c19e53ee4
Bruce Ingalls <brucei@guo.media>
2021-03-07 03:40:25 -0500

    upgraded, fixed build errors

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Layout from '../components/layout/Layout';
 
-export default () => (
+const NotFound = () => (
   <Layout>
     <h1>Not Found</h1>
     <p>You just hit a page that doesn&#39;t exist...</p>
   </Layout>
 );
+
+export default NotFound;
