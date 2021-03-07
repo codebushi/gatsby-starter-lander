5ba0184841dda0d636a31571c40d3f0b40bfb08e
Bruce Ingalls <brucei@guo.media>
2021-03-06 22:58:44 -0500

    audit clean; no js console errors

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Layout from '../components/layout/Layout';
+
+export default () => (
+  <Layout>
+    <h1>Not Found</h1>
+    <p>You just hit a page that doesn&#39;t exist...</p>
+  </Layout>
+);
