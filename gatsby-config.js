26250ac8c1f79d048a37bd0d738e607412bee31a
DeviousLab <deviouslab@gmail.com>
2021-07-21 22:47:31 +0400

    Added SEO components to site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
 module.exports = {
+  siteMetadata: {
+    title: `Tanmu Project Management Services`,
+    description: `Tanmu Project Management Services and Consultancy in Dubai and the Middle East`,
+    author: `@DeviousLab`,
+    keywords: `tanmu, project, management, services, engineering, construction, handover, consultant, dubai`
+  },
   plugins: [
     {
       resolve: `gatsby-theme-codebushi`,
       options: {
         tailwindConfig: `tailwind.config.js`
       }
-    }
+    },
+    'gatsby-plugin-react-helmet',
   ]
 };
