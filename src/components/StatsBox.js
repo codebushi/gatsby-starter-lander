5d9b72882ffe85e12d7f18c584d8fc9f4acdbce3
Bruce Ingalls <bruce.ingalls@gmail.com>
2021-12-07 14:07:11 -0500

    tested with gatsby-theme-codebushi 1.1.0

--- src/components/StatsBox.js
+++ src/components/StatsBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const StatsBox = ({ primaryText, secondaryText }) => (
   <>
-    <p className="text-4xl lg:text-6xl font-semibold text-primary">{primaryText}</p>
+    <p className="text-4xl lg:text-6xl font-semibold">{primaryText}</p>
     <p className="font-semibold mb-6">{secondaryText}</p>
   </>
 );
